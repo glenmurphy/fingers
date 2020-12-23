8a453afd2c61ab35248e33d906919a6e6eefb975
Glen Murphy <glen@glenmurphy.com>
2020-12-23 11:35:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fingers
+
+This is a quick hack to let DCS use the Leap Motion and a North Loop (the
+ring thing that came with the North Focals) to press buttons.
+
+### Requirements
+
+* Leap Motion
+* A Loop from North (you can't get these anymore)
+* Deno
+* Chrome
+* A PC with Bluetooth
+
+### Installation/Usage
+
+* deno run -A --unstable fingers.mjs
+* Open ./loop/index.html in Chrome, press 'start' and pair the North Loop
+
+### Other
+
+* [OnShape Model for the Pimax/Leap mount](https://cad.onshape.com/documents/ae5a6cb30a9eb6d1e482df71/w/023af4907bc823d27392def4/e/ad8553e8c3b3b2fdd51e0683)
---
+++ fingers.mjs
@@ -0,0 +1,167 @@
+//import AutoPilot from 'https://raw.githubusercontent.com/divy-work/autopilot-deno/master/mod.ts';
+import AutoPilot from '../../autopilot/autopilot-deno/mod.ts';
+import { Buttons } from "./buttons.mjs"
+
+// id:              fb:fe:ab:25:e4:3b
+// service:         39de08dc-624e-4d6f-8e42-e1adb7d92fe1
+// characteristic:  53b2ad55-c810-4c75-8a25-e1883a081ef6
+class Fingers {
+  constructor() {
+    this.useRightHand = false;
+    
+    this.pilot = new AutoPilot();
+    this.enabled = true;
+    this.lastClickTime = 0;
+    this.start();
+    console.log("Remember to open the Bluetooth UI at ./loop/index.html");
+  }
+
+  async start() {
+    this.screenSize = await this.pilot.screenSize(); // height, width;
+
+    this.screenCenter = {
+      x : this.screenSize.width / 2,
+      y : this.screenSize.height  / 2
+    }
+
+    this.resetPoint = {
+      x : this.screenCenter.x,
+      y : this.screenCenter.y * 1.25,
+    }
+
+    // offset (mostly used to offset Leap mounting angles)
+    this.inputAngleOffset = {
+      x : 0,  // HMD is mounted pointed to the left
+      y : 5   // HMD is slightly down
+    }
+
+    // scaling angle to screen pixels / HMD fov
+    this.inputAngleScale = {
+      x : 12,
+      y : 18
+    }
+    
+    this.connect();
+    this.buttons = new Buttons(this.handleButton.bind(this));
+  }
+
+  connect() {
+    this.disconnect();
+
+    this.ws = new WebSocket("ws://localhost:6437/v7.json");
+    this.ws.onopen = this.handleOpen.bind(this);
+    this.ws.onmessage = this.handleMessage.bind(this);
+    this.ws.onclose = this.connect.bind(this);
+  }
+
+  disconnect() {
+    if (!this.ws) return;
+    try {
+      this.ws.close();
+    } catch(e) {}
+  }
+
+  toggleEnabled() {
+    if (this.enabled) {
+      console.log("Disabling...");
+      this.moveMouse(this.resetPoint.x, this.resetPoint.y);
+      this.enabled = false;
+    } else {
+      console.log("Enabling...");
+      this.enabled = true;
+    }
+  }
+
+  handleOpen() {
+    this.ws.send(JSON.stringify({"background": true}));  // optimize for running in the background
+    this.ws.send(JSON.stringify({"focused": true}));     // claim focus
+    this.ws.send(JSON.stringify({"optimizeHMD": true})); // optimize for HMDs
+  }
+
+  handleButton(type) {
+    console.log(type);
+    
+    if (type == "up")
+      this.toggleEnabled();
+    
+    if (!this.enabled)
+      return;
+    
+    if (type == "center")
+      this.pilot.click("left")
+    if (type == (this.useRightHand ? "back" : "fwd"))
+      this.pilot.click("right")
+
+    if (type == (this.useRightHand ? "fwd" : "back"))
+      this.pilot.scroll("up");
+    if (type == "down")
+      this.pilot.scroll("down");
+    
+    this.lastClickTime = new Date().getTime();
+  }
+
+  moveMouse(x, y) {
+    if (x < 0) x = 0;
+    else if (x >= this.screenSize.width) x = this.screenSize.width - 1;
+    if (y < 0) y = 0;
+    else if (y >= this.screenSize.height) y = this.screenSize.height - 1;
+
+    if (this.enabled && this.lastClickTime < new Date().getTime() - 100) {
+      this.pilot.moveMouse(x, y);
+    }
+  }
+
+  handleMessage(e) {
+    try {
+      var data = JSON.parse(e.data);
+      if (data['hands'])
+        this.handleHands(data);
+    } catch(e) {
+      console.log(e);
+      console.log("Unexpected hand data");
+    }
+  }
+
+  findPreferredHand(hands) {
+    for (const hand of hands) {
+      if (hand['type'] == (this.useRightHand ? "right" : "left")) {
+        return hand;
+      }
+    }
+  }
+
+  handleHands(data) {
+    const hand = this.findPreferredHand(data['hands']);
+    if (!hand) return;
+
+    const pointables = data['pointables'];
+    for (const pointable of pointables) {
+      if (pointable['handId'] != hand['id']) continue;
+      if (pointable['type'] != 1) continue;
+
+       // Knuckle of the index finger
+      this.handlePointer(pointable['mcpPosition']);
+      break;
+    }
+  }
+
+  handlePointer(pos) {
+    var x = -pos[0]; // horizontal position
+    var z = pos[1];  // depth
+    var y = pos[2];  // vertical position
+
+    var h = Math.atan(x / z) * 180 / Math.PI + this.inputAngleOffset.x;
+    var v = Math.atan(y / z) * 180 / Math.PI + this.inputAngleOffset.y;
+
+    if (!this.enabled)
+      return;
+
+    this.moveMouse(
+      parseInt(this.screenCenter.x + h * this.inputAngleScale.x),
+      parseInt(this.screenCenter.y + v * this.inputAngleScale.y));
+  }
+}
+
+if (import.meta.main) {
+  new Fingers();
+}
