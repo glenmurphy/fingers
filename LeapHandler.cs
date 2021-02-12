311fbfb3b35e46e3ce92eb902f68f3ec9ec208bb
Glen Murphy <glen@glenmurphy.com>
2021-02-11 21:10:00 -0800

    Move leap offset compensation into leadhandler

--- LeapHandler.cs
+++ LeapHandler.cs
@@ -4,14 +4,37 @@ using Leap;
 
 class LeapHandler
 {
+  // Angle of the leap relative to the eye forward vector in degrees; left handed rotation. You can
+  // also use this to offset the cursor position (increasing a value will push the cursor in that
+  // direction)
+  // X+ : leap is rotated down
+  // Y+ : leap rotated right
+  // Z+ : leap is rotated counterclockwise
+  private static Vector3 mountAngleOffset = new Vector3(10, 0, 0);
+
+  // Position of the leap relative to the eye center in mm
+  // X+ : leap is to the right of the eye
+  // Y+ : leap is above the eye
+  // Z+ : leap is forward of the eye
+  //
+  // Common values: (0, 62, 106) - pimax top-mounted leap
+  //                (0, -45, 68) - pimax hand tracker
+  private static Vector3 mountPositionOffset = new Vector3(0, -45, 68);
+
   private Fingers fingers;
   HandData leftHand;
   HandData rightHand;
   Leap.IController controller;
+  Vector3 mountAngleOffsetRadians;
 
   public LeapHandler(Fingers parent)
   {
     fingers = parent;
+
+    float deg2rad = ((float)Math.PI / 180);
+    mountAngleOffsetRadians = new Vector3(mountAngleOffset.X * deg2rad, 
+                                          mountAngleOffset.Y * deg2rad, 
+                                          mountAngleOffset.Z * deg2rad);
 
     leftHand = new HandData() { isLeft = true };
     rightHand = new HandData();
@@ -59,7 +82,13 @@ class LeapHandler
 
       // Convert to HandData coordinate system
       // Can also consider using StabilizedPalmPosition
-      data.pos = new Vector3(-mcp.NextJoint[0], -mcp.NextJoint[2], mcp.NextJoint[1]);
+      data.pos = Util.RotatePosition(
+        new Vector3(-mcp.NextJoint[0], -mcp.NextJoint[2], mcp.NextJoint[1]), mountAngleOffsetRadians);
+
+      data.pos.X = data.pos.X + mountPositionOffset.X; // horizontal
+      data.pos.Y = data.pos.Y + mountPositionOffset.Y; // upness
+      data.pos.Z = data.pos.Z + mountPositionOffset.Z; // depth
+
       data.isActive = true;
 
       // Actual rotation is not reliable; use a combination of X/Y pos so users can drag
